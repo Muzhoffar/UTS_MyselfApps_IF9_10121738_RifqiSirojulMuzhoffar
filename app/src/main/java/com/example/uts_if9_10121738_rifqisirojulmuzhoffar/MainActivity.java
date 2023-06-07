33dca260e7830711d194b6e983b6feb3630a433d
Muzhoffar <rmuzhoffar@gmail.com>
2023-06-07 17:25:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uts_if9_10121738_rifqisirojulmuzhoffar/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.uts_if9_10121738_rifqisirojulmuzhoffar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.uts_if9_10121738_rifqisirojulmuzhoffar.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.activity:
+                    replaceFragment(new ActivityFragment());
+                    break;
+                case R.id.gallery:
+                    replaceFragment(new GalleryFragment());
+                    break;
+                case R.id.music:
+                    replaceFragment(new MusicFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+}
