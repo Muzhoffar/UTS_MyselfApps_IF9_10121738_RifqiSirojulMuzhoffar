c9fdb536b47b35b709d7c627c348c8af03ec2695
Muzhoffar <rmuzhoffar@gmail.com>
2023-06-08 17:15:55 +0700

    update 1.2

---
+++ app/src/main/java/com/example/uts_if9_10121738_rifqisirojulmuzhoffar/AboutDialog.java
@@ -0,0 +1,19 @@
+package com.example.uts_if9_10121738_rifqisirojulmuzhoffar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class AboutDialog extends DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.custom_about_dialog,container,false);
+    }
+}
